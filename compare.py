25d11ff9462b5b858b697f8e2691944da5c4e6a0
Eric Switzer <eric.switzer@gmail.com>
2012-11-27 14:35:56 -0500

    weighted SVD cleaning

--- compare.py
+++ compare.py
@@ -4,6 +4,7 @@ import filecmp
 import subprocess
 
 cmp_path = "../analysis_IM/"
+run_vimdiff = False
 
 matches = []
 for root, dirnames, filenames in os.walk('.'):
@@ -16,6 +17,23 @@ for filename in matches:
     try:
         if not filecmp.cmp(local_file, remote_file):
             print remote_file, local_file
-            subprocess.call(["vimdiff", local_file, remote_file])
+            if run_vimdiff:
+                subprocess.call(["vimdiff", local_file, remote_file])
     except OSError:
         print "remote does not have %s" % local_file
+
+rev_matches = []
+for root, dirnames, filenames in os.walk(cmp_path):
+  for filename in fnmatch.filter(filenames, '*.py'):
+      rev_matches.append(os.path.join(root, filename))
+
+#for filename in rev_matches:
+#    local_file = filename[2:]
+#    remote_file = cmp_path + local_file
+#    print local_file, remote_file
+#    try:
+#        same = filecmp.cmp(local_file, remote_file)
+#    except OSError:
+#        print "remote does not have %s" % local_file
+
+
